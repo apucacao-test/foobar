3efcfcbe347eaa34075cbc1fe371b59352cedbe1
Alexis Georges <alexis.georges@gmail.com>
2014-03-27 12:36:19 -0700

    dupe

--- blah.js
+++ blah.js
@@ -11,3 +11,17 @@ function RepositoryCtrl($scope, $http) {
     $http.post($scope.repoURL, $scope.repository);
   }
 }
+
+function RepositoryCtrl($scope, $http) {
+  $scope.init = function(owner, repoName) {
+    $scope.owner = owner;
+    $scope.repoURL = "/api/v1/repos/" + owner + "/"  + repoName;
+    $http.get($scope.repoURL).success(function(data) {
+      $scope.repository = data;
+    });
+  };
+
+  $scope.updateKata = function() {
+    $http.post($scope.repoURL, $scope.repository);
+  }
+}

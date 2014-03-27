8b38be88fb6d12f4ac328e5a2f2d784ff322a6d7
Alexis Georges <alexis.georges@gmail.com>
2014-03-27 11:51:17 -0700

    d

--- foo.js
+++ foo.js
@@ -181,3 +181,17 @@ function FeedbackCtrl($scope, $http) {
     $http.post("/api/v1/feedback/" + token, {"helpful": helpful});
   };
 }
+
+function RepositoryCtrl($scope, $http) {
+  $scope.init = function(owner, repoName) {
+    $scope.owner = owner;
+    $scope.repoURL = "/api/v1/repos/" + owner + "/"  + repoName;
+    $http.get($scope.repoURL).success(function(data) {
+      $scope.repository = data;
+    });
+  };
+
+  $scope.updateKata = function() {
+    $http.post($scope.repoURL, $scope.repository);
+  }
+}

604afc6f9da3e0f30b67b969f548f79cb9159346
Alexis Georges <alexis.georges@gmail.com>
2014-03-27 11:35:54 -0700

    dup

--- foo.js
+++ foo.js
@@ -1,3 +1,17 @@
+function RepositoryCtrl($scope, $http) {
+  $scope.init = function(owner, repoName) {
+    $scope.owner = owner;
+    $scope.repoURL = "/api/v1/repos/" + owner + "/"  + repoName;
+    $http.get($scope.repoURL).success(function(data) {
+      $scope.repository = data;
+    });
+  };
+
+  $scope.updateKata = function() {
+    $http.post($scope.repoURL, $scope.repository);
+  }
+}
+
 function foo(x) { return x + 1; }
 
 function RepositoriesCtrl($scope, RepoService) {

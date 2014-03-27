953f1e3cb398bd9a70441001e11c50e4946d3dc2
Alexis Georges <alexis.georges@gmail.com>
2014-03-27 12:33:20 -0700

    blah

---
+++ blah.js
@@ -0,0 +1,13 @@
+function RepositoryCtrl($scope, $http) {
+  $scope.init = function(owner, repoName) {
+    $scope.owner = owner;
+    $scope.repoURL = "/api/v1/repos/" + owner + "/"  + repoName;
+    $http.get($scope.repoURL).success(function(data) {
+      $scope.repository = data;
+    });
+  };
+
+  $scope.updateKata = function() {
+    $http.post($scope.repoURL, $scope.repository);
+  }
+}
